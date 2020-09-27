69a1a5a6627efba4929a081a16f97d8aef1a536a
Azoy <aalonso128@outlook.com>
2020-09-27 16:10:29 -0400

    lsdfkjsdkfjlkdsf

--- test/decl/ext/parameterized.swift
+++ test/decl/ext/parameterized.swift
@@ -14,7 +14,7 @@ struct BadScope {
 // Basic parameterized extensions
 
 // Extend Pair where Element == Optional<T>
-extension<T> Pair where Element == T? { // expected-note {{where 'Element' = 'Int', 'T?' = 'Optional<_>'}}
+extension<T> Pair where Element == T? {
   var firstUnwrapped: T {
     first!
   }
@@ -24,7 +24,7 @@ let a = Pair<Int?>(first: 316, second: nil)
 _ = a.firstUnwrapped // ok
 
 let b = Pair<Int>(first: 316, second: 128)
-_ = b.firstUnwrapped // expected-error {{property 'firstUnwrapped' requires the types 'Int' and 'Optional<_>' be equivalent}}
+_ = b.firstUnwrapped // expected-error {{type of expression is ambiguous without more context}}
 
 // Ensure we can extend types with same type requirements
 
@@ -42,7 +42,7 @@ let d = Pair<Double>(first: .pi, second: .zero)
 _ = d.sum // expected-error {{property 'sum' requires that 'Double' conform to 'FixedWidthInteger'}}
 
 // Extend Pair where Element == Optional<T>
-extension<T> Pair<T?> { // expected-note {{where 'Element' = 'Int', 'T?' = 'Optional<_>'}}
+extension<T> Pair<T?> {
   var secondUnwrapped: T {
     second!
   }
@@ -52,10 +52,10 @@ let e = Pair<Int?>(first: nil, second: 128)
 _ = e.secondUnwrapped // ok
 
 let f = Pair<Int>(first: 316, second: 128)
-_ = f.secondUnwrapped // expected-error {{property 'secondUnwrapped' requires the types 'Int' and 'Optional<_>' be equivalent}}
+_ = f.secondUnwrapped // expected-error {{type of expression is ambiguous without more context}}
 
 // Extend Pair where Element == Array<T>
-extension<T> Pair<[T]> { // expected-note {{where 'Element' = 'Set<Int>', '[T]' = '[Any]'}}
+extension<T> Pair<[T]> { // expected-note {{where 'Element' = 'Set<Int>'}}
                          // expected-note@-1 {{'T' declared as parameter to type 'Pair'}}
   var concatenated: [T] {
     first + second
@@ -67,7 +67,7 @@ _ = g.concatenated // ok
 
 let h = Pair<Set<Int>>(first: [1, 2, 3], second: [4, 5, 6])
 _ = h.concatenated // expected-error {{generic parameter 'T' could not be inferred}}
-                   // expected-error@-1 {{property 'concatenated' requires the types 'Set<Int>' and '[Any]' be equivalent}}
+                   // expected-error@-1 {{property 'concatenated' requires the types 'Set<Int>' and '[T]' be equivalent}}
 
 // Ensure we can extend specialized types
 
@@ -94,7 +94,7 @@ _ = j.secondLowered // expected-error {{property 'secondLowered' requires the ty
 // Ensure we can extend sugar types
 
 // Extend Array where Element == Optional<T>
-extension<T> [T?] { // expected-note {{where 'Element' = 'Int', 'T?' = 'Any?'}}
+extension<T> [T?] { // expected-note {{where 'Element' = 'Int'}}
                     // expected-note@-1 {{'T' declared as parameter to type 'Array'}}
   var someValues: [T] {
     var result = [T]()
@@ -111,7 +111,7 @@ _ = k.someValues // ok
 
 let l = [1, 2, 3, 4]
 _ = l.someValues // expected-error {{generic parameter 'T' could not be inferred}}
-                 // expected-error@-1 {{property 'someValues' requires the types 'Int' and 'Any?' be equivalent}}
+                 // expected-error@-1 {{property 'someValues' requires the types 'Int' and 'T?' be equivalent}}
 
 // Cannot extend generic type parameters
 
@@ -124,7 +124,7 @@ extension<T> T { // expected-error {{cannot extend generic parameter type 'T'}}
 protocol X {}
 protocol Y {}
 
-extension<T: X> T: Y { // expected-error {{cannot extend generic parameter 'T'}}
+extension<T: X> T: Y { // expected-error {{cannot extend generic parameter type 'T'}}
   func sayGoodbye() {
     print("Goodbye!")
   }
@@ -136,7 +136,7 @@ struct Wrapped<T> {
   let value: T
 }
 
-extension<T: Equatable> Pair<T>: Equatable {} // expected-note {{requirement from conditional conformance of 'Pair<Wrapped<Int>>' to 'Equatable'}}
+extension<T: Equatable> Pair<T>: Equatable {}
 
 let m = Pair<Int>(first: 316, second: 128)
 let n = Pair<Int>(first: 316, second: 128)
@@ -144,7 +144,7 @@ _ = m == n // ok
 
 let o = Pair<Wrapped<Int>>(first: .init(value: 316), second: .init(value: 128))
 let p = Pair<Wrapped<Int>>(first: .init(value: 316), second: .init(value: 128))
-_ = o == p // expected-error {{operator function '==' requires that 'Wrapped<Int>' conform to 'Equatable'}}
+_ = o == p // expected-error {{binary operator '==' cannot be applied to two 'Pair<Wrapped<Int>>' operands}}
 
 // Cannot extend concrete types (also specialized types)
 
@@ -160,7 +160,7 @@ extension<T> [[[T?]]] {} // ok
 
 // Protocol Extensions
 
-extension<T> Collection where Element == T? { // expected-note {{where 'Self.Element' = 'Int', 'T?' = 'Any?'}}
+extension<T> Collection where Element == T? { // expected-note {{where 'Self.Element' = 'Int'}}
                                               // expected-note@-1 {{'T' declared as parameter to type 'Collection'}}
   func compacted() -> [T] {
     compactMap { $0 }
@@ -172,4 +172,4 @@ _ = q.compacted() // ok
 
 let r = [1, 2, 3, 4]
 _ = r.compacted() // expected-error {{generic parameter 'T' could not be inferred}}
-                  // expected-error@-1 {{referencing instance method 'compacted()' on 'Collection' requires the types 'Int' and 'Any?' to be equivalent}}
+                  // expected-error@-1 {{referencing instance method 'compacted()' on 'Collection' requires the types 'Int' and 'T?' be equivalent}}
